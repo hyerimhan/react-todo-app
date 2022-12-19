291c222a5d56464fe09818316f2f1cd7720f6311
hyerimhan <hnhyerim@gmail.com>
2022-12-19 21:18:34 +0900

    feat: Create List Component

--- src/component/Lists.js
+++ src/component/Lists.js
@@ -1,24 +1,8 @@
 import React from 'react'
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import List from './List';
 
 const Lists = ({ todoData, setTodoData }) => {
-
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter((data) => data.id !== id);
-    console.log(newTodoData)
-    setTodoData(newTodoData)
-  }
-
-
-  const handleCompleteChange = (id) => {
-    const newTodoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed
-      }
-      return data;
-    })
-    setTodoData(newTodoData)
-  }
 
   const handleEnd = (result) => {
     console.log(result);
@@ -50,26 +34,18 @@ const Lists = ({ todoData, setTodoData }) => {
                     index={index}
                   >
                     {(provided, snapshot) => (
-                      <div
+                      <List 
                         key={data.id}
-                        {...provided.draggableProps}
-                        ref={provided.innerRef}
-                        {...provided.dragHandleProps}
-                        className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"} flex items-center w-full justify-between px-4 py-1 my-2 text-gray-600  border rounded`}>
-                        <div className='items-center'>
-                          <input type="checkbox" defaultChecked={data.completed}
-                            onChange={() => handleCompleteChange(data.id)}
-                          />{" "}
-                          <span className={data.completed ? "line-through" : ""}>
-                            {data.title}
-                          </span>
-                        </div>
-                        <div className='items-center'>
-                          <button onClick={() => handleClick(data.id)}>X</button>
-                        </div>
-                      </div>
+                        id={data.id}
+                        title={data.title}
+                        completed={data.completed}
+                        todoData={todoData}
+                        setTodoData={setTodoData}
+                        provided={provided}
+                        snapshot={snapshot}
+                      />
                     )}
-                  </Draggable>
+                  </Draggable> 
                 ))}
               {provided.placeholder}
             </div>
